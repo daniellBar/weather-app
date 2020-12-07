5c70aedc558e62de1d0a3cf23d730f7d4b0ab41e
LAPTOP-TO9F8GT3\daniel <d.bar6531@gmail.com>
2020-12-07 12:05:54 +0200

    Updates

---
+++ precache-manifest.cdf7b10d9775693210bc1a605cb565e8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bf737db9be066d38bf15c0b6461337",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "9b38e1ced60aa75419cf",
+    "url": "/weather-app/static/css/main.fa40698f.chunk.css"
+  },
+  {
+    "revision": "2418bec31cb8fa3dcd7e",
+    "url": "/weather-app/static/js/2.f32455aa.chunk.js"
+  },
+  {
+    "revision": "177dce6ef9045384a932287ff9d95cbd",
+    "url": "/weather-app/static/js/2.f32455aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b38e1ced60aa75419cf",
+    "url": "/weather-app/static/js/main.8c4b1409.chunk.js"
+  },
+  {
+    "revision": "6217e7c726a47d063da4",
+    "url": "/weather-app/static/js/runtime-main.8ce43dfd.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/weather-app/static/media/15.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/weather-app/static/media/16.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/weather-app/static/media/17.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/weather-app/static/media/41.421c13f1.png"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/weather-app/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/weather-app/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/weather-app/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/weather-app/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
